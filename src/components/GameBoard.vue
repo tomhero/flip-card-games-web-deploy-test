91b3b17f20f063e89e231c9af048af2aa92629b4
Chayut <chayut.191@gmail.com>
2019-12-27 14:04:27 +0700

    Finishup gameboard and card css

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,14 +1,21 @@
 <template>
   <section class="section board">
     <div class="container">
-      <button>Start Game</button>
-      <hr />
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+          <p class="bd-notification is-primary has-text-centered">
+            <button class="button is-primary is-large">Game Start</button>
+          </p>
+        </div>
+      </div>
+      <br />
       <div class="columns is-gapless is-mobile is-multiline">
         <div class="column is-3-desktop is-6-mobile is-4-tablet" v-for="index in 12" :key="index">
-          <div class="memory-card has-text-centered" data-framework="aurelia">
-            <div class="front-face"><h1>{{index}}</h1></div>
-            <div class="back-face"><h1>{{index}}</h1></div>
-            <!-- <img class="back-face" src="https://pngimage.net/wp-content/uploads/2018/06/sample-png-images-1.png" alt="JS Badge" /> -->
+          <div class="memory-card has-text-centered">
+            <div class="front-face">
+              <h1>{{index}}</h1>
+            </div>
+            <div class="back-face"></div>
           </div>
         </div>
       </div>
@@ -17,7 +24,11 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -32,20 +43,99 @@ export default {};
 
 .memory-card {
   width: 10rem;
-  height: 12.5rem;
+  height: 13rem;
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 3rem;
   position: relative;
   transform: scale(1);
   transform-style: preserve-3d;
-  transition: transform .5s;
-  box-shadow: 1px 1px 1px rgba(0,0,0,.3);
+  transition: transform 0.5s;
+  box-shadow: 4px 8px 25px 0.5px rgba(100, 100, 100, 0.5);
+
+  .back-face {
+    background: repeating-linear-gradient(
+        0deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        45deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        90deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        90deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        67.5deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        157.5deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        157.5deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        67.5deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      repeating-linear-gradient(
+        45deg,
+        hsla(54, 0%, 88%, 0.05) 0px,
+        hsla(54, 0%, 88%, 0.05) 1px,
+        transparent 1px,
+        transparent 16px
+      ),
+      linear-gradient(90deg, hsl(224, 72%, 31%), hsl(224, 72%, 31%));
+    color: rgba(241, 241, 230, 0.315);
+  }
+
+  .front-face {
+    background: rgb(89, 236, 193);
+    background: radial-gradient(
+      circle,
+      rgba(89, 236, 193, 1) 0%,
+      rgba(0, 189, 213, 1) 38%,
+      rgba(0, 123, 196, 1) 89%
+    );
+    color: #f1f1e6;
+  }
 }
 
 .memory-card:active {
   transform: scale(0.97);
-  transition: transform .2s;
+  transition: transform 0.2s;
 }
 
 .memory-card.flip {
@@ -55,13 +145,11 @@ export default {};
 .front-face,
 .back-face {
   font-size: 7rem;
-  line-height: 11rem;
+  line-height: 12rem;
   width: 100%;
   height: 100%;
   padding: 20px;
   position: absolute;
-  border-radius: 5px;
-  background: rgb(85, 166, 231);
   backface-visibility: hidden;
 }
 
