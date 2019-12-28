c1ba52fbff4ca8a729d608f13a3b77f73e598383
Chayut <chayut.191@gmail.com>
2019-12-28 16:48:26 +0700

    Fix https problem

--- src/service/gameScoreService.js
+++ src/service/gameScoreService.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 const host = process.env.VUE_APP_API_HOST || '35.227.148.145'
-const port = process.env.VUE_APP_API_PORT || '9000'
+const port = process.env.VUE_APP_API_PORT
 const apiKey = process.env.VUE_APP_API_KEY || 'be511834-5b8c-480f-98f0-8b741d3a4d65'
 const apiVersion = process.env.VUE_APP_API_VERSION || 'v1'
 
-const apiUrl = `http://${host}:${port}/api/${apiVersion}/highscore/${apiKey}`
+const apiUrl = `https://${host}${port ? ':'+port : ''}/api/${apiVersion}/highscore/${apiKey}`
 
 export function getGlobelHighestScore() {
   return fetch(apiUrl, { mode: 'cors' }).then(response => response.json())
